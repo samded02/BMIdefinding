ebe71efa8cfac3cf207434f1b51c87b1b4033aaf
Ismayil <kazovkavkaz@gmail.com>
2025-01-18 17:27:06 +0400

    second

--- script.js
+++ script.js
@@ -6,23 +6,32 @@ let netice = ''
 
 if (bmi < 18) {
     netice = "Siz, çəki azlığından əziyyət çəkirsiniz"
+    normalWeight = (`normal cekiye catmaq ucun ${(21 - bmi) * (hundurluk * hundurluk)} kg artirin`)
 } else if (bmi < 20) {
     netice = "Sizin çəkiniz azdır. Lakin sağlamlıq üçün zərəli deyil."
+    normalWeight = (`normal cekiye catmaq ucun ${(21 - bmi) * (hundurluk * hundurluk)} kg artirin`)
 } else if (bmi < 26) {
     netice = "Sizin çəkiniz normaldır. Təbriklər III"
+    normalWeight = ''
 } else if (bmi < 28) {
     netice = "Sizin artıq çəkiniz var"
+    normalWeight = (`normal cekiye catmaq ucun ${(bmi - 25) * (hundurluk * hundurluk)} kg azaldin`)
 } else if (bmi < 31) {
     netice = "Siz, 1-ci dərəcəli artıq çəkidən əziyyət çəkirsini"
+    normalWeight = (`normal cekiye catmaq ucun ${(bmi - 25) * (hundurluk * hundurluk)} kg azaldin`)
 } else if (bmi < 36) {
     netice = "Siz, 2-ci dərəcəli artıq çəkidən əziyyət çəkirsiniz"
+    normalWeight = (`normal cekiye catmaq ucun ${(bmi - 25) * (hundurluk * hundurluk)} kg azaldin`)
 } else if (bmi < 41) {
     netice = "Siz, 3-cü dərəcəli artıq çəkidən əziyyət çəkirsiniz"
+    normalWeight = (`'normal cekiye catmaq ucun' ${(bmi - 25) * (hundurluk * hundurluk)} kg azaldin`)
 } else if (41 < bmi) {
     netice = "Siz, 4-cü dərəcəli artıq çəkidən əziyyət çəkirsiniz"
+    normalWeight = (`normal cekiye catmaq ucun ${(bmi - 25) * (hundurluk * hundurluk)} kg azaldin`)
 } else{
     netice = "Sizin başınızda problem var"
 } 
 
 
-document.getElementById('netice').innerHTML = netice
+document.getElementById('bmi').innerHTML = netice
+document.getElementById('minus').innerHTML = normalWeight
